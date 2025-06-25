e74f863cf6b647c36ef458fef3921adb8887cee3
Gjurgjica Mladenova <djurdjicamladenovska2003@gmail.com>
2025-06-25 12:54:21 +0200

    Made changes

--- emt-lab-B-frontend/src/axios/axios.js
+++ emt-lab-B-frontend/src/axios/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const baseURL = process.env.VITE_API_BASE_URL || "http://localhost:9090/api"
+
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:9090/api",
+    baseURL: baseURL,
     headers: {
         "Content-Type": "application/json",
     },
