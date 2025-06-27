828f3fd5a0111eb94c3e2460d80045161b384bab
Gjurgjica Mladenova <djurdjicamladenovska2003@gmail.com>
2025-06-27 21:44:20 +0200

    Update CI config, Dockerfiles, environment settings, axios baseURL, and ingress manifests

--- emt-lab-B-frontend/src/axios/axios.js
+++ emt-lab-B-frontend/src/axios/axios.js
@@ -1,6 +1,23 @@
+// import axios from "axios";
+//
+// const baseURL = process.env.VITE_API_BASE_URL || "http://localhost:9090/api"
+//
+// const axiosInstance = axios.create({
+//     baseURL: baseURL,
+//     headers: {
+//         "Content-Type": "application/json",
+//     },
+// });
+//
+// export default axiosInstance;
+
 import axios from "axios";
 
-const baseURL = process.env.VITE_API_BASE_URL || "http://localhost:9090/api"
+
+const baseURL =
+    import.meta.env.MODE === 'development'
+        ? 'http://localhost:9090/api' // docker-compose
+        : 'http://backend.local/api'; // kubernetes ingress
 
 const axiosInstance = axios.create({
     baseURL: baseURL,
